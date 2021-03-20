7560932805b9a00ce0760904776a0e8d567dd38b
Edward Spencer <epfspencer@yahoo.com>
2021-03-20 22:13:33 +0000

    Add method to get inventory items bounding box
    Items can be captured by index. The return tuple is the bounding
    box of the item's clickable area.

---
+++ tests/test_grab_slots.py
@@ -0,0 +1,23 @@
+import time
+from os.path import dirname, join
+
+import client
+from screen_tools import Screen
+
+
+if __name__ == '__main__':
+    c = client.Client('RuneLite')
+    s = Screen()
+    c.set_rect()
+    c._client.activate()
+
+    t = time.time()
+    x0, y0 = c.inventory.get_slot_bbox(0)[:2]
+    x27, y27 = c.inventory.get_slot_bbox(27)[2:]
+
+    img = s.grab_screen(x0, y0, x27, y27)
+    path = join(dirname(__file__), '..', 'data', 'test.png')
+    s.save_img(img, path)
+    t = time.time() - t
+
+    print(f'Got screen in {round(t, 2)} seconds')
