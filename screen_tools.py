05a56553149ed823a855fadb9cf875485a69b28b
Edward Spencer <epfspencer@yahoo.com>
2021-03-20 17:42:18 +0000

    Add module to manage reading from screen
    Also set up a test to verify that it's able to capture the current
    client's window.
    Having some issues with dual monitors, especially because my second
    monitor is a different resolution to the first. I can screen grab from
    it, but the image isn't the correct size. As long as I screengrab from
    the primary monitor it works fine through.

---
+++ screen_tools.py
@@ -0,0 +1,49 @@
+import win32gui
+import win32ui
+import win32con
+import numpy
+from PIL import Image
+
+
+class Screen(object):
+
+    def grab_screen(self, x1=0, y1=0, x2=0, y2=0):
+        """
+        Grab an area of the screen and return as numpy array
+        """
+
+        # TODO: support for getting screen shot from second monitor
+        hwin = win32gui.GetDesktopWindow()
+
+        width = x2 - x1 + 1
+        height = y2 - y1 + 1
+
+        hwindc = win32gui.GetWindowDC(hwin)
+        srcdc = win32ui.CreateDCFromHandle(hwindc)
+        memdc = srcdc.CreateCompatibleDC()
+
+        # create a blank bitmap image and bind to DC
+        bmp = win32ui.CreateBitmap()
+        bmp.CreateCompatibleBitmap(srcdc, width, height)
+        memdc.SelectObject(bmp)
+
+        # copy the screen into buffer
+        memdc.BitBlt((0, 0), (width, height), srcdc, (x1, y1), win32con.SRCCOPY)
+        signedIntsArray = bmp.GetBitmapBits(True)
+
+        # convert to numpy array
+        img = numpy.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (height, width, 4)
+
+        # cleanup
+        srcdc.DeleteDC()
+        memdc.DeleteDC()
+        win32gui.ReleaseDC(hwin, hwindc)
+        win32gui.DeleteObject(bmp.GetHandle())
+
+        # NOTE: this image is BGRA
+        return img
+
+    def save_img(self, img, path):
+        img = Image.fromarray(img)
+        img.save(path)
