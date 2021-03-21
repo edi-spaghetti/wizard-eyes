40f5b72140b9b3a2bdbb4f1be99ae155bc409a11
Edi Spaghetti <44098060+edi-spaghetti@users.noreply.github.com>
2021-03-21 19:31:39 +0000

    Add deposit inventory button

---
+++ tests/test_grab_deposit_inventory.py
@@ -0,0 +1,29 @@
+import time
+import pyautogui
+from os.path import dirname, join
+
+import client
+from screen_tools import Screen
+
+
+if __name__ == '__main__':
+    c = client.Client('RuneLite')
+    s = Screen()
+    c.set_rect()
+    c._client.activate()
+
+    t = time.time()
+    img = s.grab_screen(*c.bank.utilities.deposit_inventory.get_bbox())
+    path = join(dirname(__file__), '..', 'data', 'test.png')
+    s.save_img(img, path)
+    t = time.time() - t
+
+    pyautogui.moveTo(*c.bank.utilities.deposit_inventory.get_bbox()[:2])
+    # TODO: assert mouseover text says 'Deposit inventory / 1 more options'
+    time.sleep(1)
+
+    pyautogui.moveTo(*c.bank.utilities.deposit_inventory.get_bbox()[2:])
+    # TODO: assert mouseover text says 'Deposit inventory / 1 more options'
+    time.sleep(1)
+
+    print(f'Got screen in {round(t, 2)} seconds')
