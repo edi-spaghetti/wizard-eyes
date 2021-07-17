fb29ee535d318ded12a31f2f9376fa1a837bda42
Edi Spaghetti <44098060+edi-spaghetti@users.noreply.github.com>
2021-07-17 10:24:34 +0100

    Super basic logout function
    We're currently using it so infrequently that it didn't warrant proper
    integration into the game objects for what I wanted to use it for.
    Excuses I know.. I'll map it out properly later.

--- script_utils.py
+++ script_utils.py
@@ -1,7 +1,9 @@
 import sys
 import time
+import random
 
 import keyboard
+import pyautogui
 
 
 def safety_catch(c, msg_length):
@@ -13,3 +15,29 @@ def safety_catch(c, msg_length):
         return True
     elif keyboard.is_pressed('p'):
         exit(1)
+
+
+def logout(c):
+
+    # TODO: add logout buttons to client as proper game objects
+    pause = random.random() * 60
+    time.sleep(pause)
+
+    bbox = c.get_bbox()
+    pyautogui.moveTo(bbox[2] - 20, bbox[1] + 40)
+
+    time.sleep(0.1)
+    pyautogui.click()
+
+    pause = random.random() * 5
+    time.sleep(pause)
+    pyautogui.moveTo(c.get_bbox()[2] - 100,
+                     c.get_bbox()[3] - 80)
+
+    time.sleep(0.1)
+    pyautogui.click()
+
+    sys.stdout.write('Logged out\n')
+    sys.stdout.flush()
+
+    exit()
