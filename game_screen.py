f785e9d0a9114ad6e026781c48e85c7a369c9c3a
Edi Spaghetti <44098060+edi-spaghetti@users.noreply.github.com>
2022-03-18 13:35:17 +0000

    Move NPC class to game screen module
    Also refactored npc_coordinates into a main method so i don't keep
    getting shadowed variables warnings which make things hard to debug.

--- game_screen.py
+++ game_screen.py
@@ -1,3 +1,6 @@
+import time
+from uuid import uuid4
+
 import cv2
 
 from game_objects import GameObject
@@ -76,3 +79,83 @@ class Player(GameObject):
         ty2 = ty1 + h - 1
 
         return tx1, ty1, tx2, ty2
+
+
+class NPC(GameObject):
+
+    def __init__(self, client, parent, name, v, w, x, y, z, tile_base=1):
+        super(NPC, self).__init__(client, parent)
+
+        self.id = uuid4().hex
+        self.key = v, w, x, y, z
+        self.name = name
+        self.updated_at = time.time()
+        self.checked = False
+        # TODO: add to configuration
+        self.tile_base = tile_base
+
+    @property
+    def mm_x(self):
+        """top left X pixel on the minimap"""
+        mm = self.client.minimap.minimap
+        mm_x1 = mm.get_bbox()[0]
+        x1 = self.client.get_bbox()[0]
+        x = self.key[0]
+
+        nx = int(mm_x1 - x1 + mm.config['width'] / 2 + x * mm.tile_size)
+        return nx
+
+    @property
+    def mm_y(self):
+        """top left Y pixel on the minimap"""
+        mm = self.client.minimap.minimap
+        mm_y1 = mm.get_bbox()[1]
+        y1 = self.client.get_bbox()[1]
+        y = self.key[1]
+
+        ny = int(mm_y1 - y1 + mm.config['height'] / 2 + y * mm.tile_size)
+        return ny
+
+    def ms_bbox(self):
+        """
+        Calculate the bounding box for the current NPC on the main screen.
+        Bounding box is global, so to get pixel positions relative to client
+        image do the following,
+
+        .. code-block:: python
+
+            x1, y1, x2, y2 = npc.ms_bbox()
+            cx1, cy1, _, _ = client.get_bbox()
+
+            # relative to image top left
+            rx1 = x1 - cx1
+            ry1 = y1 - cy1
+
+        """
+
+        # collect components
+        player = self.client.game_screen.player
+        cx1, cy1, cx2, cy2 = player.static_bbox()
+        px, py, _, _ = player.tile_bbox()
+        # convert relative to static bbox so we can use later
+        px = px - cx1 + 1
+        py = py - cy1 + 1
+
+        t_height, t_width = player.templates['player_marker'].shape
+        x, y = self.key[:2]
+
+        # calculate values
+        x1 = cx1 + px + (t_width * x)
+        y1 = cy1 + py + (t_height * y)
+        x2 = x1 + (t_width * 2) - x
+        y2 = y1 + (t_height * 2) - y
+
+        return x1, y1, x2, y2
+
+    def refresh(self):
+        self.checked = False
+
+    def update(self, key):
+        self.key = key
+        self.updated_at = time.time()
+        self.checked = True
