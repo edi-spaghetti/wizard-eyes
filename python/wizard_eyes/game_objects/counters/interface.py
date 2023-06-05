6f2fd701c7d2fd4a7f5eca712e6191e193f0aa4d
Edward Spencer <edward.spencer.community@gmail.com>
2023-06-05 13:07:16 +0100

    Add dyanmic menu for counters
    This includes things like potions timeouts, slayer tasks, opponent
    information etc. Anything in the top left really.

---
+++ python/wizard_eyes/game_objects/counters/interface.py
@@ -0,0 +1,21 @@
+from ...dynamic_menus.interface import AbstractInterface
+from .icon import CountersInterfaceIcon
+
+
+class CountersInterface(AbstractInterface):
+
+    PATH_TEMPLATE = '{root}/data/game_screen/counters/{name}.npy'
+
+    def __init__(self, client, widget):
+        super().__init__(
+            client, widget, config_path='mouse_options.counters.interface'
+        )
+
+    @property
+    def icon_class(self):
+        return CountersInterfaceIcon
+
+    def draw(self):
+
+        if f'counters_interface_bbox' in self.client.args.show:
+            self.draw_bbox()
