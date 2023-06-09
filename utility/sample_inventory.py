bcd1c4d8dcfe2b39e781d9286f6a0e8bb416bb11
Edward Spencer <edward.spencer.community@gmail.com>
2023-06-09 07:31:13 +0100

    Improve sample inventory utility
     - Auto mask templates
     - Optionally copy template & mask to tabs folder

--- utility/sample_inventory.py
+++ utility/sample_inventory.py
@@ -1,14 +1,18 @@
+import argparse
 import time
-from os.path import dirname, join, exists
+from os.path import dirname, exists, basename
 from os import makedirs
+import re
+from shutil import rmtree, copy2
 
 import cv2
 import numpy
 
 from wizard_eyes import client
+from wizard_eyes.file_path_utils import get_root
 
 
-def sample(item_name, s, c):
+def sample(item_name, s, c, idx, overwrite=False):
     t = time.time()
 
     # grab whole screen and sample from it for each slot
@@ -21,10 +25,13 @@ def sample(item_name, s, c):
         x1, y1, x2, y2 = c.inventory.slots[i].get_bbox()
         # numpy arrays are stored rows x columns, so flip x and y
         slot_img = img[y1-y:y2-y, x1-x:x2-x]
+        # create a rough mask - will need to be verified & refined
+        mask = cv2.inRange(slot_img, (38, 50, 59, 255), (46, 56, 66, 255))
+        mask = cv2.bitwise_not(mask)
 
         # prepare path to save template into
         path = c.inventory.slots[i].PATH_TEMPLATE.format(
-                root=join(dirname(__file__), '..'),
+                root=get_root(),
                 index=i,
                 name=item_name
             )
@@ -33,9 +40,27 @@ def sample(item_name, s, c):
 
         # first save a colour copy for reference
         cv2.imwrite(path.replace('.npy', '.png'), slot_img)
+        cv2.imwrite(path.replace('.npy', '_mask.png'), mask)
         # process and save the numpy array
         processed_img = c.inventory.slots[i].process_img(slot_img)
         numpy.save(path, processed_img)
+        numpy.save(path.replace('.npy', '_mask.npy'), mask)
+
+    if idx is not None:
+        for ext in ('.npy', '.png', '_mask.npy', '_mask.png'):
+            source = c.inventory.slots[idx].PATH_TEMPLATE.format(
+                root=get_root(),
+                index=idx,
+                name=item_name
+            ).replace('.npy', ext)
+            target = (f'{get_root()}/data/tabs/{basename(source)}'
+                      .replace('.npy', ext))
+
+            if exists(target) and not overwrite:
+                print(f'{target} already exists, skipping')
+                continue
+
+            copy2(source, target)
 
     t = time.time() - t
 
@@ -44,17 +69,40 @@ def sample(item_name, s, c):
 
 def main():
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clear', action='store_true', default=False)
+    parser.add_argument('--overwrite', action='store_true', default=False)
+    args = parser.parse_args()
+
     c = client.Client('RuneLite')
     s = c.screen
 
     for i in range(len(c.inventory.slots)):
         c.inventory.set_slot(i)
 
+        path = c.inventory.slots[i].PATH_TEMPLATE.format(
+            root=get_root(),
+            index=i,
+            name='_'
+        )
+        path = dirname(path)
+
+        if args.clear and exists(path):
+            rmtree(path)
+
     print('Press enter on blank item to exit')
+    print('Follow item name with index to save to tabs directory '
+          '(will overwrite existing without warning!)')
     while True:
         item_name = input('Item Name: ')
+        idx = None
+        match = re.match('([^()]+)\s?\(?([0-9]+)?\)?', item_name)
+        if match:
+            item_name = match.group(1)
+            idx = int(match.group(2))
+
         if item_name:
-            sample(item_name, s, c)
+            sample(item_name, s, c, idx, overwrite=args.overwrite)
         else:
             break
 
