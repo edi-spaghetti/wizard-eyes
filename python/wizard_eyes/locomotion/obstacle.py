8b856356186907ab8449ea4fd0d258ad961bbc30
Edi Spaghetti <44098060+edi-spaghetti@users.noreply.github.com>
2023-06-24 22:48:14 +0100

    Add walker class mixin for applications
    Handles travelling along a course of obstacles, or simply generate a
    path and travelling along it.

---
+++ python/wizard_eyes/locomotion/obstacle.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from ..game_entities.entity import GameEntity
+
+
+
+@dataclass
+class Obstacle:
+    """Dataclass to represent a single entity that needs to be clicked as
+    part of a route. It could be an agility obstacle, or it could be a ladder
+    to another floor of a building."""
+
+    map_name: str
+    label: str
+    mouse_text: str
+    timeout: float
+    success_label: str
+    width: int = 1
+    height: int = 1
+    fail_idx: int = None
+    fail_label: str = None
+    entity: GameEntity = None
+    offsets: tuple = None
+    routes: tuple = None
+    additional_delay: float = None
+    mouse_thresh_lower: int = None
+    allow_partial: bool = True
+    multi: int = 1
+    min_confidence: float = 0.0
+    custom_click_box: bool = False
+    fallback_confidence_after: float = -float('inf')
+    fallback_confidence_before: float = -float('inf')
---
+++ python/wizard_eyes/locomotion/traveller.py
@@ -0,0 +1,262 @@
+from abc import ABC
+from random import random, uniform, choice
+from typing import List, Union, Dict
+
+from .obstacle import Obstacle
+from ..application import Application
+
+
+class Traveller(Application, ABC):
+    """Application mixin class to support moving around the map."""
+
+    COURSE: List[Obstacle] = []
+
+    LONG_OBSTACLE_TIMEOUT = 60
+    MED_OBSTACLE_TIMEOUT = 6
+    SHORT_OBSTACLE_TIMEOUT = 2
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.entities = []
+        self.obstacle_id = None
+        self.path: Union[List, None] = None
+        self.entities_mapping: Dict[str, str] = {}
+
+    @property
+    def current_obstacle(self) -> Obstacle:
+        """Get the next obstacle we need to cross."""
+        return self.COURSE[self.obstacle_id]
+
+    @property
+    def next_obstacle(self) -> Obstacle:
+        """Get the next obstacle we need to cross"""
+        # NOTE: this course is not a loop, so we don't need to worry about
+        #       modulo on the obstacle id
+        return self.COURSE[self.obstacle_id + 1]
+
+    def obstacle_post_script(self):
+        """
+        set a small afk timer after we pass an obstacle, so we don't
+        appear to always do an immediate action.
+
+        Also update entities now that the map has been swapped entities need
+        to be re-updated to pick up their new locations.
+        """
+
+        afk = random()
+        if afk < 0.0001:
+            self.afk_timer.add_timeout(uniform(
+                self.client.TICK,
+                self.client.TICK * self.LONG_OBSTACLE_TIMEOUT
+            ))
+        elif afk < 0.01:
+            self.afk_timer.add_timeout(uniform(
+                self.client.TICK,
+                self.client.TICK * self.MED_OBSTACLE_TIMEOUT
+            ))
+        elif afk < 0.1:
+            self.afk_timer.add_timeout(uniform(
+                self.client.TICK,
+                self.client.TICK * self.SHORT_OBSTACLE_TIMEOUT
+            ))
+
+        self.client.logger.debug(f'Calculated afk to: {afk:.4f}')
+
+    def increment_obstacle_id(self):
+        """Increment obstacle id by one."""
+        self.obstacle_id = self.obstacle_id + 1
+        self.obstacle_post_script()
+
+    def reset_course(self):
+        """Clear the timeout for the all obstacles and start at index 0."""
+        for obstacle in self.COURSE:
+            obstacle.entity.clear_timeout()
+        self.obstacle_id = 0
+
+    def remove_course(self):
+        """Remove all obstacles from the course, and remove entities from
+        entities list."""
+        for obstacle in self.COURSE:
+            entity = obstacle.entity
+            idx = self.entities = self.entities.index(entity)
+            self.entities.pop(idx)
+        self.COURSE = []
+
+    def setup_course(self):
+        """Create entities for all obstacles in the course."""
+
+        gps = self.client.minimap.minimap.gps
+
+        for obstacle in self.COURSE:
+            map_ = gps.load_map(obstacle.map_name, set_current=False)
+            # TODO: NPCs as obstacles.
+            entity = self._setup_game_entity(
+                obstacle.label, map_=map_
+            )
+            entity.tile_width = obstacle.width
+            entity.tile_height = obstacle.height
+            if obstacle.offsets:
+                entity.x1_offset = obstacle.offsets[0]
+                entity.y1_offset = obstacle.offsets[1]
+                entity.x2_offset = obstacle.offsets[2]
+                entity.y2_offset = obstacle.offsets[3]
+
+            # TODO: custom clickbox?
+
+            # cache entity to obstacle item and application class for updating
+            obstacle.entity = entity
+            self.entities.append(entity)
+
+            # add mapping for entity to map name, so we can update the entity
+            # only when we're on that map.
+            self.entities_mapping[obstacle.label] = obstacle.map_name
+
+    def update_path(self):
+
+        mm = self.client.minimap.minimap
+        gps = self.client.minimap.minimap.gps
+        pxy = gps.get_coordinates()
+        px, py = pxy
+
+        # trim nodes in path we're close enough to or already passed
+        trim_idx = 0
+        for i, entity in enumerate(self.path):
+            node = entity.get_global_coordinates()
+            dist = mm.distance_between(node, pxy)
+            if dist > (mm.orb_radius - 10) / mm.tile_size:
+                break
+
+            trim_idx = i
+        self.path = self.path[trim_idx:]
+
+        # check the first node in the path is actually reachable
+        for entity in self.path:
+            node = entity.get_global_coordinates()
+            dist = mm.distance_between(node, (px, py), as_pixels=True)
+            if dist > mm.orb_radius:
+                # reset the path to recalculate
+                self.path = []
+            break
+
+        # update each node in path with updated keys
+        for entity in self.path:
+            x, y = entity.get_global_coordinates()
+            key = (int((x - px) * mm.tile_size),
+                   int((y - py) * mm.tile_size))
+            entity.update(key=key)
+
+    def travel_path(self, speed):
+
+        mm = self.client.minimap.minimap
+        gps = self.client.minimap.minimap.gps
+
+        if self.path:
+            checkpoint = self.path[0]
+            node = checkpoint.get_global_coordinates()
+            dist = mm.distance_between(node, gps.get_coordinates())
+            if not speed or not checkpoint.clicked:
+                checkpoint.click(
+                    # divide 2 assumes running
+                    dist * self.client.TICK / 2,
+                    dist * self.client.TICK / 2 * (random() + 1),
+                    bbox=checkpoint.mm_bbox(),
+                    pause_before_click=True,
+                )
+                self.afk_timer.add_timeout(uniform(.2, .6))
+                self.msg.append(f'clicked checkpoint {node}')
+            else:
+                self.msg.append(f'waiting arrive checkpoint: {node}')
+        else:
+            start = gps.current_map.find(nearest=gps.get_coordinates())
+            end = gps.current_map.label_to_node(
+                self.current_obstacle.label).pop()
+            end = gps.current_map.find(nearest=end)
+
+            if self.current_obstacle.routes:
+                checkpoint = [choice(self.current_obstacle.routes)]
+            else:
+                checkpoint = None
+
+            try:
+                path = gps.get_route(start, end, checkpoints=checkpoint)
+            except KeyError as err:
+                self.msg.append(f'failed to generate path: {err}')
+                return
+
+            # if self.current_obstacle.success_label == 'cave_3_exit':
+            #     self.client.logger.debug('trimmed last node')
+            #     path = path[:-1]
+
+            px, py = gps.get_coordinates()
+            for x, y in path:
+                key = (int((x - px) * mm.tile_size),
+                       int((y - py) * mm.tile_size))
+
+                entity = self.client.game_screen.create_game_entity(
+                    'node', 'node', key, self.client, self.client
+                )
+                entity.set_global_coordinates(x, y)
+                self.path.append(entity)
+
+            self.msg.append(f'generated path from: {start} to {end}')
+
+    def travel_course(self):
+
+        mm = self.client.minimap.minimap
+        gps = self.client.minimap.minimap.gps
+        speed = gps.calculate_average_speed(period=self.client.TICK)
+
+        x1, y1, x2, y2 = self.current_obstacle.entity.click_box()
+        if self.client.game_screen.is_clickable(
+                x1, y1, x2, y2,
+                allow_partial=self.current_obstacle.allow_partial):
+
+            dist = mm.distance_between(
+                gps.get_coordinates(),
+                self.current_obstacle.entity.get_global_coordinates()
+            )
+            # TODO: check if running
+            dist_timeout = max([dist * (self.client.TICK / 2),
+                                self.client.TICK])
+            timeout = dist_timeout + self.current_obstacle.timeout
+
+            method = None
+            if self.current_obstacle.custom_click_box:
+                method = self.current_obstacle.entity.click_box
+
+            self.path = []  # clear path since obstacle is on screen
+
+            # map swap confidence is so low at the altar that it's easy to
+            # get stuck. However the previous map confidence is also low,
+            # so we can use that to make an assessment.
+            before = self.current_obstacle.fallback_confidence_before
+            after = self.current_obstacle.fallback_confidence_after
+            map_swap_fallback = (
+                    gps.confidence <= before
+                    and self.swap_confidence <= after
+            )
+
+            if map_swap_fallback:
+                gps.load_map(self.next_obstacle.map_name)
+                node = gps.current_map.label_to_node(
+                    self.current_obstacle.success_label).pop()
+                gps.set_coordinates(*node)
+                self.msg.append(
+                    f'Obstacle fallback skip: '
+                    f'{self.current_obstacle.label}')
+                self.increment_obstacle_id()
+
+            self._teleport_with_item(
+                self.current_obstacle.entity,
+                self.next_obstacle.map_name,
+                self.current_obstacle.success_label,
+                tmin=timeout,
+                tmax=timeout * 1.5,
+                post_script=self.increment_obstacle_id,
+                mouse_text=self.current_obstacle.mouse_text,
+                multi=self.current_obstacle.multi,
+                confidence=self.next_obstacle.min_confidence,
+                method=method,
+            )
+        else:
+            self.travel_path(speed)
