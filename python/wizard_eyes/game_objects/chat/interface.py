07c31ce0c5054f6bf4294515c149683797b56b7a
Edward Spencer <edward.spencer.community@gmail.com>
2023-07-02 13:43:10 +0100

    Add draw text method for chat interfaces

--- python/wizard_eyes/game_objects/chat/interface.py
+++ python/wizard_eyes/game_objects/chat/interface.py
@@ -1,3 +1,4 @@
+import cv2
 from PIL import Image
 
 from ...dynamic_menus.interface import AbstractInterface
@@ -19,6 +20,9 @@ class ChatInterface(AbstractInterface):
 
     def read_text(self):
 
+        if not self.located:
+            return
+
         img = Image.fromarray(self.img)
         self.client.ocr.SetImage(img)
         text = str(self.client.ocr.GetUTF8Text())
@@ -26,3 +30,41 @@ class ChatInterface(AbstractInterface):
             self.text_changed_at = self.client.time
 
         self.text = text
+        self.client.add_draw_call(self.draw_text)
+
+    def draw_text(self):
+        """Draws the text on the client image."""
+
+        if not self.located:
+            return
+
+        x1, y1, x2, y2 = self.client.localise(*self.get_bbox())
+        w = x2 - x1
+        h = y2 - y1
+
+        # TODO: configurable and dynamically fitting text
+        font_size = .45
+        thickness = 1
+
+        triggers = {'*text', f'{self.widget.name}_text'}
+        if self.client.args.show.intersection(triggers):
+
+            lines = str(self.text).split('\n')
+            for i, line in enumerate(lines):
+
+                (tw, th), _ = cv2.getTextSize(
+                    text=line,
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                    fontScale=font_size,
+                    thickness=thickness,
+                )
+
+                cv2.putText(
+                    self.client.original_img,
+                    line,
+                    (int(x1 + w * 0.1), int(y1 + h * 0.1 + i * th)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    font_size,
+                    self.colour,
+                    thickness=thickness,
+                )
