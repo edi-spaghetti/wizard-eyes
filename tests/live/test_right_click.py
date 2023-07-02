7259f50824f6f8aaa87d75a37b12072e556667e2
Edward Spencer <edward.spencer.community@gmail.com>
2023-07-02 13:46:26 +0100

    Update live tests for ocr and right click
    - Right click demo now responds to user input rather than random. Much
      more useful when you need to check a specific RC menu.
    - Chat OCR has its own demo now.

--- tests/live/test_right_click.py
+++ tests/live/test_right_click.py
@@ -1,59 +1,62 @@
-from random import uniform
+import mouse
 
 from wizard_eyes.application import Application
-from wizard_eyes.game_objects.game_objects import GameObject
 
 
 class TestRightClickApp(Application):
     """Demonstrates the right click menu with OCR."""
 
-    def __init__(self, *args, **kwargs):
-        """Set up some timers to click and click away, as well as a fake object
-        we are going to click."""
-        super().__init__(*args, **kwargs)
-        self.click_timer = -float('inf')
-        self.mouse_away_timer = -float('inf')
-        self.object = GameObject(self.client, self.client)
+    def init_right_click_menu(self):
+        """Whenever user right clicks inside the client try to find a context
+        menu there."""
+        x, y = self.client.screen.mouse_xy
+        if not self.client.is_inside(x, y):
+            return
+
+        menu = self.client.set_context_menu(x, y)
+        menu.OCR_READ_ITEMS = True
+
+    def on_clicked_menu_item(self):
+        """On left-clicking any of the menu items in a right click menu,
+        the menu is destroyed. This is usually handled by
+        :func:`wizard_eyes.game_objects.right_click_menu.MenuItem.click`, but
+        we need to handle it here because the user is clicking manually."""
+
+        rc = self.client.right_click_menu
+        x, y = self.client.screen.mouse_xy
+        if not rc.located:
+            return
+
+        for i, item in enumerate(rc.items):
+            if item.is_inside(x, y):
+                rc.logger.info(f'Clicked item {i}')
+                rc.reset()
+                return
 
     def setup(self):
-        """Force args to show the bounding boxes and menu item states.
-        We want OCR to be enabled for this right click menu, so turn it on.
-        Set the fake object's bbox to be the center third of the screen.
-        This is where we'll click to create right click menus."""
+        """Force args to show the bounding boxes and menu item states. Set up
+        the event loop to check for right click actions."""
 
         self.client.args.show = {'mouse', '*bbox', '*state'}
-
-        self.client.right_click_menu.OCR_READ_ITEMS = True
-
-        x1, y1, x2, y2 = self.client.get_bbox()
-        self.object.set_aoi(
-            int(x1 + self.client.width / 3), int(y1 + self.client.height / 3),
-            int(x2 - self.client.width / 3), int(y2 - self.client.height / 3))
+        mouse.on_right_click(self.init_right_click_menu)
+        mouse.on_click(self.on_clicked_menu_item)
 
     def update(self):
         """Just update the object, if a right click menu has been set up
         updates will automatically propagate down."""
 
-        self.object.update()
+        self.client.right_click_menu.update()
 
     def action(self):
-        """Click on the center third if it's time. Click away if it's time.
-        This way we can see the right click menu in a variety of positions.
-        The menu item text is logged to the console."""
+        """Log menu item text to the console."""
 
-        if self.client.time > self.click_timer:
-            self.click_timer = self.client.time + uniform(3, 4)
-            self.mouse_away_timer = self.client.time + uniform(2, 3)
+        rc = self.client.right_click_menu
 
-            x, y = self.object.right_click(pause_before_click=True)
-            self.object.set_context_menu(x, y)
-
-        elif self.client.time > self.mouse_away_timer:
-            self.mouse_away_timer = self.click_timer
-            self.client.screen.mouse_away_object(self.client.right_click_menu)
-
-        message = ', '.join([i.value for i in self.client.right_click_menu.items])
-        self.msg.append(message)
+        if rc.located:
+            message = ' | '.join([i.value for i in rc.items])
+            self.msg.append(message)
+        else:
+            self.msg.append('Right click somewhere to locate a menu')
 
 
 def main():
